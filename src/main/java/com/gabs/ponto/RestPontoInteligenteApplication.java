dd40307e8f7fe7bb44331c5c1f16239abf94d0a1
GabsLeo <gabrieldemoraistaveira@gmail.com>
2021-11-11 20:27:00 -0300

    :tada: First Commit

---
+++ src/main/java/com/gabs/ponto/RestPontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.gabs.ponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPontoInteligenteApplication.class, args);
+	}
+
+}
